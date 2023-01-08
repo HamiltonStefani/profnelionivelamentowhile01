4d497a26960acd375e2bd58282d678d31f90c37b
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2023-01-08 08:57:08 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package exerciciosobrewhile;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int soma =0;
+		
+		while (x != 0) {
+			soma += x;
+			x = sc.nextInt();
+		}
+		System.out.println("O valor da soma é: " + soma);
+		sc.close();
+	}
+
+}
